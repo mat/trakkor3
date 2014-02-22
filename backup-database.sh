c9af2e990e03564a483a957b2af88217abd828da
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-02-22 12:14:55 +0100

    Update backup-database.sh to new S3 location.

--- backup-database.sh
+++ backup-database.sh
@@ -1,2 +1,6 @@
 
-cp /home/trakkor/rails_app/db/production.sqlite3 /tmp/production.sqlite3 && bzip2 --best -f /tmp/production.sqlite3 && s3cmd put /tmp/production.sqlite3.bz2 s3://better-idea-box/backups/ -v --mime-type='application/x-bzip' >> /tmp/backup-database.log 2>&1
+set -e
+
+cp /home/trakkor/rails_app/db/production.sqlite3 /tmp/production.sqlite3
+bzip2 --best -f /tmp/production.sqlite3
+s3cmd put /tmp/production.sqlite3.bz2 s3://better-idea-box/trakkor/ -v --mime-type='application/x-bzip' >> /tmp/backup-database.log 2>&1
