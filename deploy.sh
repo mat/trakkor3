19ef772af4f395c2b21dfa2436cc3900401851ec
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-15 14:56:44 +0100

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+
+set -e
+
+git archive master | bzip2 > sources.bz2
+scp sources.bz2 better-idea.org:/home/trakkor
+aws s3 cp sources.bz2 s3://better-idea-box/trakkor/trakkor-sources.bz2
+ssh better-idea.org "cd /home/trakkor && tar jxvf sources.bz2 -C rails_app"
+ssh better-idea.org "cd /home/trakkor/rails_app && bundle install --deployment --without development test && touch tmp/restart.txt"
