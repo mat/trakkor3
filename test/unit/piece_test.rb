1fd47f911843f5201ee97ea241af7cfa0374051c
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-15 16:21:44 +0100

    Indent test.

--- test/unit/piece_test.rb
+++ test/unit/piece_test.rb
@@ -26,12 +26,12 @@ class PieceTest < MiniTest::Unit::TestCase
     assert_equal "Google", result.text
   end
 
-#  def test_that_kitty_can_eat
-#    assert_equal "OHAI!", @meme.i_can_has_cheezburger?
-#  end
+  #  def test_that_kitty_can_eat
+  #    assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+  #  end
 
-#  def test_that_it_will_not_blend
-#    refute_match /^no/i, @meme.will_it_blend?
-#  end
+  #  def test_that_it_will_not_blend
+  #    refute_match /^no/i, @meme.will_it_blend?
+  #  end
 end
 
