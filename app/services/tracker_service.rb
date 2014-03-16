7a2b72b45108b769f80fc26ab968f247c5d34430
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-16 13:59:06 +0100

    Move update code into TrackerService.

---
+++ app/services/tracker_service.rb
@@ -0,0 +1,29 @@
+
+class TrackerService
+
+  def self.update_trackers(logger)
+    trackers = Tracker.all
+
+    trackers.each do |tracker|
+      logger.info("Updating tracker %d (%s),  fetching %s..." % [tracker.id, tracker.md5sum, tracker.uri])
+
+      old_piece = tracker.current
+      new_piece = tracker.fetch_piece
+      if(!old_piece && new_piece.text.present?)
+        logger.info("Content available for the first time: %s" % [new_piece.text.colorize(:green)])
+        new_piece.save!
+      elsif new_piece.text.present? && !old_piece.same_content(new_piece)
+        logger.info("Content changed from %s to %s" % [old_piece.text, new_piece.text.colorize(:green)])
+        new_piece.save!
+      else
+        logger.info("Content unchanged at %s" % [old_piece.text.colorize(:yellow)])
+      end
+
+      if tracker.should_notify?(old_piece,new_piece)
+        logger.info("POSTing to web hook at %s" % tracker.web_hook)
+        tracker.notify_change(old_piece, new_piece)
+      end
+    end
+  end
+
+end
--- fetch-n-save.rb
+++ fetch-n-save.rb
@@ -16,6 +16,7 @@ raise "SCRAPER_PATH (#{SCRAPER_PATH}) does not exist." unless File.exist? SCRAPE
 
 require "#{SCRAPER_PATH}/app/models/tracker.rb"
 require "#{SCRAPER_PATH}/app/models/piece.rb"
+require "#{SCRAPER_PATH}/app/services/tracker_service.rb"
 
 dbconfig = YAML::load(File.open('config/database.yml')).fetch(RAILS_ENV)
 ActiveRecord::Base.establish_connection(dbconfig)  
@@ -26,28 +27,7 @@ Rails.logger = logger
 logger.info("Using #{RAILS_ENV} environment.")
 
 runtime_ms = Benchmark.realtime do
-  trackers = Tracker.all
-
-  trackers.each do |tracker|
-    logger.info("Updating tracker %d (%s),  fetching %s..." % [tracker.id, tracker.md5sum, tracker.uri])
-
-    old_piece = tracker.current
-    new_piece = tracker.fetch_piece
-    if(!old_piece && new_piece.text.present?)
-      logger.info("Content available for the first time: %s" % [new_piece.text.colorize(:green)])
-      new_piece.save!
-    elsif new_piece.text.present? && !old_piece.same_content(new_piece)
-      logger.info("Content changed from %s to %s" % [old_piece.text, new_piece.text.colorize(:green)])
-      new_piece.save!
-    else
-      logger.info("Content unchanged at %s" % [old_piece.text.colorize(:yellow)])
-    end
-
-    if tracker.should_notify?(old_piece,new_piece)
-      logger.info("POSTing to web hook at %s" % tracker.web_hook)
-      tracker.notify_change(old_piece, new_piece)
-    end
-  end
+  TrackerService.update_trackers(logger)
 end
 
 logger.info("Finished in %.2fs" % runtime_ms)
