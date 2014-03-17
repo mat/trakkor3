4276b8520a5907e886c4c2d683be3aede975c8e4
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-17 08:28:40 +0100

    Increment error count on each update run.

--- app/services/tracker_service.rb
+++ app/services/tracker_service.rb
@@ -19,6 +19,13 @@ class TrackerService
         logger.info("Content unchanged at %s" % [old_piece.text.colorize(:yellow)])
       end
 
+      err = new_piece.error
+      if err
+        tracker.error_count += 1
+        tracker.update_attribute(:error_count, tracker.error_count)
+        logger.warn("ERROR: " + err.colorize(:red))
+      end
+
       if tracker.should_notify?(old_piece,new_piece)
         logger.info("POSTing to web hook at %s" % tracker.web_hook)
         tracker.notify_change(old_piece, new_piece)
