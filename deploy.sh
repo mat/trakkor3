ae43debd3fd81b5159f8b44a9902dfdee9d9b6af
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-18 16:15:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
+#!/bin/sh
 
 set -e
 
+SHA="master"
 BZ="stuff.i/sources.bz2"
 
-git archive master | bzip2 > $BZ
+git archive $SHA | bzip2 > $BZ
 scp $BZ better-idea.org:/home/trakkor
 aws s3 cp $BZ s3://better-idea-box/trakkor/trakkor-sources.bz2
 ssh better-idea.org "cd /home/trakkor && tar jxvf sources.bz2 -C rails_app"
 ssh better-idea.org "cd /home/trakkor/rails_app && bundle install --deployment --without development test && RAILS_ENV=production bundle exec rake db:migrate && touch tmp/restart.txt"
+git tag deployed $SHA -f
+
 
