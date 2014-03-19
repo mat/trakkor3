85e1d4ed516c2f8c703ed3a74f2bb5423e77d7a7
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-19 10:50:28 +0100

    Update deploy to include ssh username.

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@ set -e
 
 SHA="master"
 BZ="stuff.i/sources.bz2"
+SSH_USER="trakkor"
 
 git archive $SHA | bzip2 > $BZ
-scp $BZ better-idea.org:/home/trakkor
+scp $BZ $SSH_USER@better-idea.org:/home/trakkor
 aws s3 cp $BZ s3://better-idea-box/trakkor/trakkor-sources.bz2
-ssh better-idea.org "cd /home/trakkor && tar jxvf sources.bz2 -C rails_app"
-ssh better-idea.org "cd /home/trakkor/rails_app && bundle install --deployment --without development test && RAILS_ENV=production bundle exec rake db:migrate && touch tmp/restart.txt"
+ssh $SSH_USER@better-idea.org "cd /home/trakkor && tar jxvf sources.bz2 -C rails_app"
+ssh $SSH_USER@better-idea.org "cd /home/trakkor/rails_app && bundle install --deployment --without development test && RAILS_ENV=production bundle exec rake db:migrate && touch tmp/restart.txt"
 git tag deployed $SHA -f
 
 
