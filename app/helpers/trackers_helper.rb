d6d21310c4c1da917a6ed807b7aa30a3a2f42832
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-19 18:50:26 +0100

    Generate correct iso8601 representation for timeago.

--- app/helpers/trackers_helper.rb
+++ app/helpers/trackers_helper.rb
@@ -2,7 +2,8 @@ module TrackersHelper
 
   def timeago(time, options = {})
     options[:class] ||= "timeago"
-    content_tag(:abbr, time.to_s, options.merge(:title => time.getutc.iso8601)) if time
+    utc_timestamp = time.utc.iso8601
+    content_tag(:abbr, utc_timestamp, options.merge(:title => utc_timestamp)) if time
   end
 
   def htidy(text)
