56b41cf36e38839b20d7f611e9f198de99cfd931
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-20 10:43:49 +0100

    Make Piece's method private, remove extract_text.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,3 @@
-
 class Piece < ActiveRecord::Base
   belongs_to :tracker
 
@@ -11,49 +10,6 @@ class Piece < ActiveRecord::Base
     self.error = error
 
     self
-  end
-
-  def self.fetch_text(uri, xpath)
-    service_url = "http://xpfetcher.herokuapp.com"
-    service_params = {url: uri, xpath: xpath}
-
-    begin
-      response = Piece.fetch_from_uri(service_url, service_params)
-    rescue => e
-      error = "Error: #{e.to_s}"
-      return ["", error]
-    end
-
-    if response.success?
-      response_json = JSON.parse(response.body)
-      text = response_json.fetch("content")
-      error = response_json.fetch("error")
-      return [text, error]
-    elsif response.code.to_i == 0
-      error = "Error: #{response.code} #{response.options.fetch(:return_code)}"
-      return ["", error]
-    else
-      error = "Error #{response.code}"
-      return ["", error]
-    end
-  end
-
-  def Piece.fetch_from_uri(uri_str, params)
-    Typhoeus::Request.get(uri_str, followlocation: true, connecttimeout: 2_000, maxredirs:4, timeout: 10_000,  params: params)
-  end
-
-  def Piece.extract_elem(data,xpath)
-    puts data.class
-    # puts "XPATH: %s" % xpath
-    res = data.at(xpath)
-    puts "RES: %s" % res
-    res
-  end
-
-  def Piece.extract_text(data, xpath)
-    elem = Piece.extract_elem(data, xpath)
-    return nil unless elem
-    [elem.to_original_html, elem.inner_text]
   end
 
   def Piece.extract_with_parents(doc, xpath)
@@ -86,6 +42,35 @@ class Piece < ActiveRecord::Base
   end
 
   private
+  def self.fetch_text(uri, xpath)
+    service_url = "http://xpfetcher.herokuapp.com"
+    service_params = {url: uri, xpath: xpath}
+
+    begin
+      response = Piece.fetch_from_uri(service_url, service_params)
+    rescue => e
+      error = "Error: #{e.to_s}"
+      return ["", error]
+    end
+
+    if response.success?
+      response_json = JSON.parse(response.body)
+      text = response_json.fetch("content")
+      error = response_json.fetch("error")
+      return [text, error]
+    elsif response.code.to_i == 0
+      error = "Error: #{response.code} #{response.options.fetch(:return_code)}"
+      return ["", error]
+    else
+      error = "Error #{response.code}"
+      return ["", error]
+    end
+  end
+
+  def Piece.fetch_from_uri(uri_str, params)
+    Typhoeus::Request.get(uri_str, followlocation: true, connecttimeout: 2_000, maxredirs:4, timeout: 10_000,  params: params)
+  end
+
   def Piece.tidy_tabby_lines(str)
     str.gsub(/\n\t+\n/, "\n\n")
   end
@@ -93,4 +78,13 @@ class Piece < ActiveRecord::Base
   def Piece.tidy_multiple_nl(str)
     str.gsub(/\n\n+/, "\n")
   end
+
+  def Piece.extract_elem(data,xpath)
+    puts data.class
+    # puts "XPATH: %s" % xpath
+    res = data.at(xpath)
+    puts "RES: %s" % res
+    res
+  end
+
 end
