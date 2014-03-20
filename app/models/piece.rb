40b77e0aeb25e24ce4cea987d64383e1825aa5ea
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-03-20 10:46:46 +0100

    Use https to call xpath service.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -43,7 +43,7 @@ class Piece < ActiveRecord::Base
 
   private
   def self.fetch_text(uri, xpath)
-    service_url = "http://xpfetcher.herokuapp.com"
+    service_url = "https://xpfetcher.herokuapp.com"
     service_params = {url: uri, xpath: xpath}
 
     begin
